9bcc3fa1645322ae31688cc723beee43524813b8
Daniel Rodrigues <coeng.daniel@gmail.com>
2022-07-25 15:54:44 -0400

    mudando para o shell zsh da forma correta

--- clearlinux-setup/zsh-setup.sh
+++ clearlinux-setup/zsh-setup.sh
@@ -1,6 +1,6 @@
 # Setup Zsh and OhMyZsh
 sudo swupd bundle-add zsh
-sudo chsh -s /bin/zsh
+chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # The file ~/.aliases can be used for both zsh and bash
