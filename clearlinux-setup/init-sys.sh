e62e4568d4fb5b819f762537836eef3ab918efb3
Daniel Rodrigues <coeng.daniel@gmail.com>
2022-07-25 14:24:00 -0400

    adicionando alias | colocando os flatpaks em varias linhas pra ficar mais legível

--- clearlinux-setup/init-sys.sh
+++ clearlinux-setup/init-sys.sh
@@ -9,17 +9,23 @@ touch ~/.bash_aliases
 echo -e "alias searchpkg='flatpak search'" >> .bash_aliases
 echo -e "alias addpkg='flatpak install flathub -y --system'" >> .bash_aliases
 echo -e "alias s1='ssh drodrigues@192.168.2.57'" >> .bash_aliases
+echo -e "alias code='flatpak run com.visualstudio.code'" >> .bash_aliases
 
 # Setup ~/.ssh
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 
-# Install NVM
+# Install NVM (Node Version Manager)
 sudo swupd bundle-add wget # By default, clear linux don't have wget
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # Installing tools
-flatpak install flathub -y --system com.anydesk.Anydesk com.axosoft.GitKraken com.brave.Browser com.jetbrains.DataGrip com.visualstudio.code org.flameshot.Flameshot
+flatpak install flathub -y --system com.anydesk.Anydesk     
+flatpak install flathub -y --system com.axosoft.GitKraken
+flatpak install flathub -y --system com.brave.Browser
+flatpak install flathub -y --system com.jetbrains.DataGrip
+flatpak install flathub -y --system com.visualstudio.code
+flatpak install flathub -y --system org.flameshot.Flameshot
 
 # Install Docker & Docker Compose
 sudo swupd bundle-add containers-basic
