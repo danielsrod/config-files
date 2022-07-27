a13280164fdaecc9a618d7528258736aba9ae0e4
Daniel Rodrigues <coeng.daniel@gmail.com>
2022-07-27 17:37:36 -0400

    script para instalar o vscode feito

--- clearlinux-setup/vscode-setup.sh
+++ clearlinux-setup/vscode-setup.sh
@@ -1,17 +1,18 @@
 sudo echo "'-.-'"
-
+echo ">>> Downloading VSCode <<<"
 wget https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-x64 -O ./vscode.tar.gz
-
+echo ">>> Unpacking VSCode <<<"
 tar -xvf vscode.tar.gz
-
+echo ">>> Remove tar file downloaded <<<"
 rm -rf vscode.tar.gz
-
+echo ">>> Rename the folder to vscode <<<"
 mv VSCode-linux-x64 vscode
-
+echo ">>> Moving the folder vscode to /opt/"
 sudo mv vscode /opt
-
-sudo touch vscode.desktop
-
+echo ">>> Creating a file vscode.desktop <<<"
+touch vscode.desktop
+echo ">>> Appeding text Desktop Entry in the file vscode.desktop  <<<"
+pwd
 echo "[Desktop Entry]
 Name=Visual Studio Code
 Comment=A code editor from microsoft.
@@ -21,11 +22,9 @@ Terminal=false
 Type=Application
 Categories=Development;Editor;Vscode;
 StartupNotify=true" >> vscode.desktop 
-
+echo ">>> Moving the file vscode.desktop to /usr/share/applications  <<<"
 sudo mv vscode.desktop /usr/share/applications
+echo ">>> Creating alias [code=/opt/vscode/code]  <<<"
+echo "alias code='/opt/vscode/code'" >> ~/.aliases
 
-echo "alias code='/opt/vscode/code'" >> .aliases
-
-rm -rf vscode-setup.sh
-
-source ~/.zshrc
+echo "Please, reboot your terminal to apply changes"
